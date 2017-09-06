7d0f0fde2bbb746be7229b78b78427391570de37
Martin Feil <foss2017@sgdw.de>
2017-09-06 23:17:43 +0200

    We have a proper license now

--- glight.py
+++ glight.py
@@ -1,36 +1,30 @@
 #!/usr/bin/env python
 
 '''
-  *  The MIT License (MIT)
-  *
-  *  g2x3 v0.1 Copyright (c) 2016 SGDW
-  *
-  *  Permission is hereby granted, free of charge, to any person obtaining a copy
-  *  of this software and associated documentation files (the "Software"), to deal
-  *  in the Software without restriction, including without limitation the rights
-  *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-  *  copies of the Software, and to permit persons to whom the Software is
-  *  furnished to do so, subject to the following conditions:
-  *
-  *  The above copyright notice and this permission notice shall be included in all
-  *  copies or substantial portions of the Software.
-  *
-  *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-  *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-  *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-  *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-  *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-  *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-  *  SOFTWARE.
-  *
-  *
-  *
-  * This software was inspired by:
-  *
-  * G213Colors by SebiTimeWaster
-  * https://github.com/SebiTimeWaster/G213Colors
-  *
-  * Thank you!
+GLight controls LEDs of some Logitech devices
+
+Copyright (C) 2017  Martin Feil aka. SGDW
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+Acknowledgements:
+
+* This software was inspired by G213Colors written SebiTimeWaster
+  https://github.com/SebiTimeWaster/G213Colors
+  Thank you!
+
 '''
 
 # pylint: disable=C0326
@@ -65,7 +59,7 @@ app_version = "0.1"
 
 default_time = 1000
 
-# /usr/lib/python2.7/dist-packages/
+# Note to self: /usr/lib/python2.7/dist-packages/
 
 class UsbConstants(object):
     HID_REQ_SET_REPORT=0x09
